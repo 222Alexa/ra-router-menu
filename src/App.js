0bc97d50d85fbd729d135324fc33f9e7615d3f44
Alexandra Titova <auto-steklo@mail.ru>
2022-11-08 15:36:17 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import "./main.css";
+import "./components/Menu/menu.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./components/Menu/Menu";
+import HomePage from "./components/Pages/HomePage";
+import DriftPage from "./components/Pages/DriftPage";
+import TimeAttackPage from "./components/Pages/TimeAttackPage";
+import ForzaPage from "./components/Pages/ForzaPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Menu />
+        <div className="page">
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/drift" element={<DriftPage />} />
+            <Route path="/timeattack" element={<TimeAttackPage />}/>
+            <Route path="/forza" element={<ForzaPage />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
